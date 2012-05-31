29f069fbce1645149baf2b647a8463eeeadf23fe
James Lal <james@lightsofapollo.com>
2012-05-31 12:43:12 -0700

    Added html output of test results

---
+++ create-test-results.js
@@ -0,0 +1,17 @@
+//this code is probably horrible but it will get the job done.
+var fsPath = require('path'),
+    fs = require('fs'),
+    MOCHA_RESULTS = 'mocha-test-results.html',
+    template = fs.readFileSync('templates/test.html', 'utf8');
+
+function Create(gaiaDir, title, outputFile) {
+  var results = fs.readFileSync(fsPath.join(gaiaDir, MOCHA_RESULTS), 'utf8'),
+      output = template;
+
+  output = output.replace('{{TITLE}}', title);
+  output = output.replace('{{BODY}}', results);
+
+  fs.writeFileSync(outputFile, output, 'utf8');
+}
+
+module.exports = Create;
--- on_cmd_test.js
+++ on_cmd_test.js
@@ -2,18 +2,26 @@ var botio = require(process.env['BOTIO_MODULE']);
 require('shelljs/global');
 
 cmd = [
+  'DISPLAY=:99',
   'GAIA_PORT=8777',
   'REPORTER=TAP',
   'TEST_OUTPUT=stdout',
   'TEST_FAST=0',
   'TEST_AGENT_SERVER=ws://50.116.11.35:8789',
-  'B2G_HOME=/data/bot.io/b2g',
-  'GAIA_DOMAIN=pr1.gaiamobile.org',
-  'tools/ci/unit/pr.sh emulator'
+  'B2G_HOME=/data/botio/b2g-desktop/obj-x86_64-unknown-linux-gnu/',
+  'GAIA_DOMAIN=prdb2g.gaiamobile.org',
+  'GAIA_PORT=:8080',
+  'tools/ci/unit/b2g-desktop.sh'
 ];
 
+exec('Xvfb :99 &');
 exec('make update-common');
 
 exec(cmd.join(' '), {silent: false, async: true}, function(err, output) {
-  botio.message('Finished running tests');
+  require('./create-test-results')(
+    botio.private_dir,
+    'B2G Desktop Results',
+    botio.public_dir + '/b2g-desktop-results.html'
+  );
+  botio.message('B2G Desktop: ' + botio.public_url + '/b2g-desktop-results.html');
 });
